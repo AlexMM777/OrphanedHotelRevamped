a3147f72e62c09c6a954b3cdac96ebd1a9948998
AlexMM777 <alex.mendoza.mondragon7777777@gmail.com>
2023-08-27 16:38:13 -0500

    Initial upload

--- Assets/Scripts/SettingsButtonAI.cs
+++ Assets/Scripts/SettingsButtonAI.cs
@@ -5,11 +5,13 @@ using UnityEngine.Audio;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class SettingsButtonAI : MonoBehaviour
 {
     public GameObject mainMenu, quit, settings;
     Resolution[] resolutions;
+    private Resolution[] reversedResolutions;
     public TMP_Dropdown resolutionDropdown;
     public AudioMixer audioMixer;
     public TMP_Dropdown graphics;
@@ -17,31 +19,37 @@ public class SettingsButtonAI : MonoBehaviour
 
     void Start()
     {
-        mainMenu.SetActive(true);
-        quit.SetActive(false);
-        settings.SetActive(false); 
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-        List<string> options = new List<string>();
-        int currentResolutionIndex = 0;
-        for(int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-        graphics.value = PlayerPrefs.GetInt("GraphicQuality");
+        //mainMenu.SetActive(true);
+        //quit.SetActive(false);
+        //settings.SetActive(false); 
+        CreateResDropdown();
     }
 
     void Update()
     {
 
+    }
+
+    public void CreateResDropdown()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        reversedResolutions = resolutions.Reverse();
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        graphics.value = PlayerPrefs.GetInt("GraphicQuality");
     }
 
     public void LoadScene(string sceneToLoad)
